c7323006a847d747bd23a5cfc578905722d9cdce
eraliabdinazarov <eraliabdinazarov22@gmail.com>
2023-03-13 00:56:34 +0500

    change admin files

--- app/admin.py
+++ app/admin.py
@@ -3,7 +3,7 @@ from .models import DollarCourse
 
 
 class DollarCourseAdmin(admin.ModelAdmin):
-    list_display = ('id', 'dollar_course', 'dollar', 'som', 'created')
+    list_display = ('id', 'dollar_course', 'som_input', 'dollar_input', 'dollar', 'som', 'created')
     list_display_links = ('id', 'dollar_course')
     list_filter = ('created', )
 
