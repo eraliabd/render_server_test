a1591d57c9316604afe0090747c2413ecfbdeee5
eraliabdinazarov <eraliabdinazarov22@gmail.com>
2023-03-13 00:55:38 +0500

    change models files

--- app/models.py
+++ app/models.py
@@ -2,9 +2,11 @@ from django.db import models
 
 
 class DollarCourse(models.Model):
-    dollar_course = models.PositiveBigIntegerField()
-    som = models.PositiveBigIntegerField()
-    dollar = models.PositiveBigIntegerField()
+    dollar_course = models.CharField(max_length=255)
+    som_input = models.CharField(max_length=255, null=True, blank=True)
+    dollar_input = models.CharField(max_length=255, null=True, blank=True)
+    som = models.CharField(max_length=255)
+    dollar = models.CharField(max_length=255)
     created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
