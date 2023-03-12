563dd8f408885be3e5e75ff524da9dde4158979d
eraliabdinazarov <eraliabdinazarov22@gmail.com>
2023-03-12 20:19:53 +0500

    templates files

--- app/views.py
+++ app/views.py
@@ -1,6 +1,41 @@
 from django.shortcuts import render
+
+import config.settings
 from .models import DollarCourse
+import requests
+import datetime
 
 
 def main(request):
-    return render(request, 'index.html')
+    current_date = datetime.date.today()
+
+    API_KEY = config.settings.env.str("API_KEY")
+    currency = "USD"
+    url = f"https://v6.exchangerate-api.com/v6/{API_KEY}/pair/{currency}/UZS"
+
+    response = requests.get(url)
+    # print(response.status_code)
+    course = response.json()['conversion_rate']
+    print(f"Bugungi kurs: 1AQSH dollori = {course} so'm")
+
+    if request.method == 'POST':
+        som_input = request.POST.get('som_to_dollar', 0)
+        dollar_input = request.POST.get('dollar_to_som', 0)
+        print(type(som_input), type(dollar_input))
+
+        som_to_dollar = float(som_input) / course
+        dollar_to_som = course * float(dollar_input)
+
+        context = {
+            "course": course,
+            "som_to_dollar": f"{som_to_dollar:.3f}",
+            "dollar_to_som": f"{dollar_to_som:.3f}",
+            "date": current_date
+        }
+        return render(request, 'index.html', context)
+    else:
+        context = {
+            "course": course,
+            "date": current_date
+        }
+        return render(request, 'index.html', context)
